4ddcea4ae079a3dfe1c642671bb34717365d5bda
madorPele1 <madorpele1@gmail.com>
2025-06-15 18:05:42 +0300

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import LomdotPage from './components/LomdotPage';
-import TorotPage from './components/TorotPage';
-import BotPage from './components/BotPage';
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LomdotPage from "./components/LomdotPage";
+import TorotPage from "./components/TorotPage";
+import BotPage from "./components/BotPage";
+import "@fontsource/assistant";
+import "@fontsource/assistant/800.css";
+import "@fontsource/assistant/700.css";
+import "@fontsource/assistant/200.css";
+import "@fontsource/assistant/500.css";
 function App() {
   return (
     <Router>
-      <div style={{ textAlign: 'center', padding: '2rem' }}>
-        <h1>קטגוריות</h1>
-        <nav style={{ display: 'flex', justifyContent: 'center', gap: '2rem' }}>
+      <div style={{ textAlign: "center", padding: "1rem" }}>
+        <img style={{filter:" drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.5))"}} src="/contentWeb/media/pakar.svg" alt="pakar" width="100" />
+        <h1 style={{}}>אתר חירום פיקודי</h1>
+        <nav style={{ display: "flex", justifyContent: "center", gap: "2rem" }}>
           <Link to="/lomdot">לומדות</Link>
           <Link to="/torot">תורות</Link>
           <Link to="/bot">בוט חילוץ</Link>
--- src/components/LomdotPage.jsx
+++ src/components/LomdotPage.jsx
@@ -1,38 +1,94 @@
 const lomdot = [
   {
-    title: 'לומדה 1',
-    image: '/barcode1.png',
-    link: 'https://example.com/lomda1',
+    title: 'לומדת חומ"ס לגדודים',
+    image: "/contentWeb/media/gdudim.png",
+    link: "https://madorpele1.github.io/HomasLomda/",
   },
   {
-    title: 'לומדה 2',
-    image: '/barcode2.png',
-    link: 'https://example.com/lomda2',
+    title: 'מילון פקע"ר',
+    image: "/contentWeb/media/milon.png",
+    link: "https://madorpele1.github.io/pakarDictionary/",
   },
   {
-    title: 'לומדה 3',
-    image: '/barcode3.png',
-    link: 'https://example.com/lomda3',
+    title: 'לומדת הע"ס',
+    image: "/contentWeb/media/haas.png",
+    link: "https://madorpele1.github.io/Aloha-Lomda/",
   },
   {
-    title: 'לומדה 4',
-    image: '/barcode4.png',
-    link: 'https://example.com/lomda4',
+    title: 'לומדת יועמ"ש',
+    image: "/contentWeb/media/yoamash.png",
+    link: "https://madorpele1.github.io/Yoamash/",
+  },
+  {
+    title: 'פנקס חומ"ס',
+    image: "/contentWeb/media/pinkas.png",
+    link: "https://madorpele1.github.io/homas-notebook/",
   },
 ];
 
 export default function LomdotPage() {
   return (
     <div>
-      <h2>לומדות</h2>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '2rem', justifyContent: 'center' }}>
+      <h2
+        style={{
+          fontSize: "9vmin",
+          color: "#333",
+        }}
+      >
+        לומדות
+      </h2>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "2rem",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
         {lomdot.map((item, index) => (
-          <div key={index} style={{ border: '1px solid #ccc', padding: '1rem' }}>
-            <h3>{item.title}</h3>
+          <div
+            key={index}
+            style={{
+              padding: "1rem",
+              background: "#ffffffc2",
+              borderRadius: "20px",
+              height: "30%",
+              minHeight: "50svh",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <h3
+              style={{
+                fontSize: "7.5vmin",
+                color: "#333",
+              }}
+            >
+              {item.title}
+            </h3>
+            <div>
+              <img src={item.image} alt="Barcode" width="200" />
+            </div>
             <a href={item.link} target="_blank" rel="noopener noreferrer">
-              <button>צפייה</button>
+              <button
+                style={{
+                  backgroundColor: " #13527a",
+                  color: "white",
+                  fontFamily: "assistant",
+                  fontWeight: "600",
+                  fontSize: "6vmin",
+                  borderRadius: "20px",
+                  width: "25vw",
+                  height: "6vh",
+                  border: "none",
+                  margin: "2vh",
+                }}
+              >
+                צפייה
+              </button>
             </a>
-            <div><img src={item.image} alt="Barcode" width="100" /></div>
           </div>
         ))}
       </div>
